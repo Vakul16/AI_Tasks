df15ae8cf5c5a23e17f8a6e13d410c000ba89d64
Vakul16 <valusavakhul@gmail.com>
2021-07-08 21:31:25 +0530

    imported new page recursive and context api

--- advance_filters/src/Pages/HomePage.js
+++ advance_filters/src/Pages/HomePage.js
@@ -1,12 +1,28 @@
 import React from "react";
-// import AdvanceFilters from "../Components/AdvanceFilters";
+import AdvanceFilters from "../Components/AdvanceFilters";
+import MovieList from "../Context/MovieList";
+import { MovieProvider } from "../Context/MovieContext";
+import AddMovie from "../Context/AddMovie";
 import Filters from "../DisplayDynamically/Filters";
+import RecursiveFilter from "../Components/Recursion/RecursiveFilter";
+import URI from "urijs";
+import { treeData } from "../Components/Recursion/RecursiveData";
 const HomePage = () => {
+  // console.log(window.location.href);
+  // var uri = new URI(window.location.href);
+  // console.log(uri);
+  // uri.query({ foo: "bar", hello: ["world", "mars"] });
+
   return (
     <>
-      <h5 className="heading">Advance Filters</h5>
+      {/* <h5 className="heading">Advance Filters</h5> */}
       {/* <AdvanceFilters /> */}
-      <Filters />
+      <RecursiveFilter data={treeData} />
+      {/* <Filters /> */}
+      {/* <MovieProvider>
+        <AddMovie />
+        <MovieList />
+      </MovieProvider> */}
     </>
   );
 };
