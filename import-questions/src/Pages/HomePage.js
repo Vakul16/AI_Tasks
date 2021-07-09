7089269860496c5ffd280b1382fd63aa9484234b
Vakul16 <valusavakhul@gmail.com>
2021-07-09 18:05:05 +0530

    Implemented search, button and table

---
+++ import-questions/src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DataTable from '../Components/DataTable'
+
+const HomePage = () => {
+    return (
+        <div>
+            <DataTable />
+        </div>
+    )
+}
+
+export default HomePage
